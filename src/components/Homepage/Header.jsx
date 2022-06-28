07e749696dad1492bd0e9a72d1bdbffc0f3e5dba
YaraMohammed98 <yara.mohammed.samir@gmail.com>
2022-06-29 01:42:43 +0200

    startpage updated

--- src/components/Homepage/Header.jsx
+++ src/components/Homepage/Header.jsx
@@ -5,11 +5,11 @@ import Navbar from './../Navbar';
 function Header() {
   return(
     
-    <div className='video-container'>
+    <div className='video-container '>
       <Navbar/>
         <video src={video} autoPlay loop muted />
-        <div className="content my-5">
-            <h1 className='header-txt my-5'>Start investing right now!</h1>
+        <div className="content">
+            <h1 className='header-txt'>Start investing right now!</h1>
             <p className='col-7'>In June last year, TVL sat at about <span className='main-color'>$1 billion worth</span>  of cryptocurrency deposited in DeFi applications. Today, the TVL
                  figure exceeds the <span className='main-color'>$60 billion mark</span>. We specialize in capitalizing on this incredible growth.    
             Join us and benefit from our profitable investments.</p>
